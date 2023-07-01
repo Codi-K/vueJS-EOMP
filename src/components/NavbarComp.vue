79e8046748cc57d99d0b8c7f756219bd2b044484
Codi-K <codikader7@gmail.com>
2023-07-01 12:34:55 +0200

    Navbar done

---
+++ src/components/NavbarComp.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <router-link class="navbar-brand" to="/">
+      <img src="https://cdn.betterttv.net/emote/61480095b63cc97ee6d261ac/2x.gif" loading="lazy" alt="Logo">
+    </router-link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <router-link class="nav-link" aria-current="page" to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/about">About</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/resume">Resume</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/projects">Projects</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/testimonials">Testimonial</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/contact">Contact</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,19 +3,36 @@ import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: "/about",
+    name: "about",
+    component: () => import("../views/AboutView.vue"),
+  },
+  {
+    path: "/resume",
+    name: "resume",
+    component: () => import("../views/ResumeView.vue"),
+  },
+  {
+    path: "/projects",
+    name: "projects",
+    component: () => import("../views/ProjectsView.vue"),
+  },
+  {
+    path: "/testimonials",
+    name: "testimonials",
+    component: () => import("../views/TestimonialsView.vue"),
+  },
+  {
+    path: "/contact",
+    name: "contact",
+    component: () => import("../views/ContactView.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
