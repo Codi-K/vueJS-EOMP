1875ab0238e35e0649be88320e2ef0ef14459372
Codi-K <codikader7@gmail.com>
2023-07-06 16:26:35 +0200

    about done

--- src/components/AboutComp.vue
+++ src/components/AboutComp.vue
@@ -1,37 +1,39 @@
 <template>
   <div>
-    <div class="aboutd d-flex justify-content-between">
-      <div class="p-0 m-0 col-5 d-flex justify-content-center">
-        <img
-          src="https://i.postimg.cc/brrd8txr/C12-C2-Codi-Kader.jpg"
-          class="dp img-fluid rounded-circle"
-          loading="lazy"
-          alt="Profile Picture"
-        />
-      </div>
-      <div class="p-1 m-0 col-7">
-        <div class="about text-center bg-dark border border-5 rounded-5">
-          <div class="">
-            <h4 class="fw-bold">
-              I am a very honest and bubbly person. I enjoying interacting with
-              different people and learning about different things and in
-              different work enviroments.
-            </h4>
-          </div>
-          <div class="my-4">
-            <h4 class="fw-bold">
-              I am very passionate about coding and problem solving. I can get
-              very excited with certain topics and certain types of challengers.
-              I learnt about HTML, CSS, Bootstrap, JavaScript and vuejs which
-              was fun and exciting.
-            </h4>
-          </div>
-          <div>
-            <h4 class="fw-bold">
-              I have worked immediately since coming out of HighSchool and being
-              at Life Choices is my first time doing any type of studies or
-              course.
-            </h4>
+    <div class="">
+      <div class="aboutd" id="sssssssssssss">
+        <div class="p-0 m-0 d-flex justify-content-center">
+          <img
+            src="https://i.postimg.cc/brrd8txr/C12-C2-Codi-Kader.jpg"
+            class="dp img-fluid rounded-circle"
+            loading="lazy"
+            alt="Profile Picture"
+          />
+        </div>
+        <div class="p-1" id="dp_paragraph">
+          <div class="about text-center bg-dark border m-3 rounded-5">
+            <div class="">
+              <h4 class="fw-bold">
+                I am a very honest and bubbly person. I enjoying interacting
+                with different people and learning about different things and in
+                different work enviroments.
+              </h4>
+            </div>
+            <div class="my-4">
+              <h4 class="fw-bold">
+                I am very passionate about coding and problem solving. I can get
+                very excited with certain topics and certain types of
+                challengers. I learnt about HTML, CSS, Bootstrap, JavaScript and
+                vuejs which was fun and exciting.
+              </h4>
+            </div>
+            <div>
+              <h4 class="fw-bold">
+                I have worked immediately since coming out of HighSchool and
+                being at Life Choices is my first time doing any type of studies
+                or course.
+              </h4>
+            </div>
           </div>
         </div>
       </div>
@@ -43,4 +45,16 @@
 export default {};
 </script>
 
-<style scoped></style>
+<style scoped>
+#sssssssssssss {
+  margin: 2rem;
+}
+.dp {
+  height: 20rem;
+  margin: auto;
+}
+#dp_paragraph {
+  margin: auto;
+  width: 85%;
+}
+</style>
