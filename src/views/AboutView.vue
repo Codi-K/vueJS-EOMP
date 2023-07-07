8c3c935c185ab7c173158f2079923e8d5c8fbc1c
Codi-K <codikader7@gmail.com>
2023-07-07 10:01:37 +0200

    comps changed

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="about_content pt-2">
-    <div class="text-center">
-      <h1>About</h1>
-      <AboutComp/>
+  <div>
+    <div>
+      <div class="aboutd m-0 my-3" id="aboutd">
+        <div class="p-0 my-3 d-flex justify-content-center">
+          <img
+            src="https://i.postimg.cc/brrd8txr/C12-C2-Codi-Kader.jpg"
+            class=" img-fluid rounded-circle"
+            id="dp"
+            loading="lazy"
+            alt="Profile Picture"
+          />
+        </div>
+        <div class="p-0" id="dp_paragraph">
+          <div class="about text-center bg-dark border rounded-5">
+            <div class="">
+              <h4 class="fw-bold">
+                I am a very honest and bubbly person. I enjoying interacting
+                with different people and learning about different things and in
+                different work enviroments.
+              </h4>
+            </div>
+            <div class="my-4">
+              <h4 class="fw-bold">
+                I am very passionate about coding and problem solving. I can get
+                very excited with certain topics and certain types of
+                challengers. I learnt about HTML, CSS, Bootstrap, JavaScript and
+                vuejs which was fun and exciting.
+              </h4>
+            </div>
+            <div>
+              <h4 class="fw-bold">
+                I have worked immediately since coming out of HighSchool and
+                being at Life Choices is my first time doing any type of studies
+                or course.
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import AboutComp from "@/components/AboutComp.vue";
 export default {
-  components: {
-    AboutComp,
-  },
+
 };
 </script>
 
